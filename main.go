7519a798506f7177d3d01cbd00f314a26bb993d1
Alexander <117208524+PlatosCodes@users.noreply.github.com>
2023-07-18 17:11:14 -0400

    Implement Dockerfile and Docker-Compose

--- main.go
+++ main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/csv"
 	"log"
+	"os"
 
 	"github.com/PlatosCodes/MerolaStation/api"
 	db "github.com/PlatosCodes/MerolaStation/db/sqlc"
 	"github.com/PlatosCodes/MerolaStation/util"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -27,11 +30,39 @@ func main() {
 	}
 
 	// FOR ADDING TRAIN DATA
-	// ctx := &gin.Context{}
-	// LoadCSVDataToDB(ctx, server)
+	ctx := &gin.Context{}
+	loadCSVDataToDB(ctx, server)
 
 	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
+	}
+}
+
+func loadCSVDataToDB(ctx *gin.Context, server *api.Server) {
+	file, err := os.Open("train_data.csv")
+	if err != nil {
+		log.Fatalf("Cannot open '%s': %s\n", "train_data.csv", err.Error())
+	}
+	defer file.Close()
+
+	r := csv.NewReader(file)
+	r.Comma = ','
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("Cannot read '%s': %s\n", "train_data.csv", err.Error())
+	}
+
+	for _, line := range lines {
+		arg := db.CreateTrainParams{
+			ModelNumber: line[1],
+			Name:        line[2],
+		}
+
+		_, err := server.Store.CreateTrain(ctx, arg)
+		if err != nil {
+			log.Fatalf("Cannot create train: %s\n", err.Error())
+		}
 	}
 }
