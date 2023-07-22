0762f65c316549e16a06118c05029df3fc4d4b3f
Alexander <117208524+PlatosCodes@users.noreply.github.com>
2023-07-21 20:37:10 -0400

    add db docs

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+DB_URL=postgresql://root:bluecomet@localhost:5432/merola_station?sslmode=disable
+network:
+	docker network create merolastation-network
 postgres:
 	docker run --name postgres -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=bluecomet -d postgres:15-alpine
 	
@@ -10,17 +13,27 @@ dropdb:
 citext:
 	docker exec -it postgres psql --username=root merola_station -c "CREATE EXTENSION IF NOT EXISTS citext;"
 
+
 migrateup:
-	migrate -path db/migration -database "postgresql://root:bluecomet@localhost:5432/merola_station?sslmode=disable" -verbose up
+	migrate -path db/migration -database "${DB_URL}" -verbose up
 
 migrateup1:
-	migrate -path db/migration -database "postgresql://root:bluecomet@localhost:5432/merola_station?sslmode=disable" -verbose up 1
+	migrate -path db/migration -database "${DB_URL}" -verbose up 1
 
 migratedown: 
-	migrate -path db/migration -database "postgresql://root:bluecomet@localhost:5432/merola_station?sslmode=disable" -verbose down
+	migrate -path db/migration -database "${DB_URL}" -verbose down
 
 migratedown1: 
-	migrate -path db/migration -database "postgresql://root:bluecomet@localhost:5432/merola_station?sslmode=disable" -verbose down 1
+	migrate -path db/migration -database "${DB_URL}" -verbose down 1
+
+new_migration:
+	migrate create -ext sql -dir db/migration -seq $(name)
+
+db_docs:
+	dbdocs build doc/db.dbml
+
+db_schema:
+	dbml2sql --postgres -o doc/schema.sql doc/db.dbml
 
 sqlc:
 	sqlc generate
@@ -34,4 +47,4 @@ server:
 mock:
 	mockgen -package mockdb -destination db/mock/store.go github.com/PlatosCodes/MerolaStation/db/sqlc Store
 	
-.PHONY: postgres createdb dropdb citext migrateup migrateup1 migratedown migratedown1 sqlc test server mock
+.PHONY: postgres createdb dropdb citext migrateup migrateup1 migratedown migratedown1 new_migration db_docs db_schema sqlc test server mock
---
+++ doc/schema.sql
@@ -0,0 +1,115 @@
+-- SQL dump generated using DBML (dbml-lang.org)
+-- Database: PostgreSQL
+-- Generated at: 2023-07-22T00:33:27.926Z
+
+CREATE TABLE "users" (
+  "id" bigserial PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "email" CITEXT UNIQUE NOT NULL,
+  "hashed_password" bytea NOT NULL,
+  "activated" BOOLEAN NOT NULL,
+  "password_last_changed_at" timestamptz NOT NULL DEFAULT (0001-01-01 00:00:00),
+  "version" bigint NOT NULL DEFAULT 1,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "trains" (
+  "id" bigserial PRIMARY KEY,
+  "model_number" varchar NOT NULL,
+  "name" varchar NOT NULL,
+  "value" bigint NOT NULL DEFAULT 0,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "version" bigint NOT NULL DEFAULT 1,
+  "last_edited_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "collection_trains" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL,
+  "train_id" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "wishlist_trains" (
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL,
+  "train_id" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "trade_offers" (
+  "id" bigserial PRIMARY KEY,
+  "offered_train" bigint NOT NULL,
+  "offered_train_owner" bigint NOT NULL,
+  "requested_train" bigint NOT NULL,
+  "requested_train_owner" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "trade_entry" (
+  "id" bigserial PRIMARY KEY,
+  "offered_train" bigint NOT NULL,
+  "offered_train_owner" bigint NOT NULL,
+  "requested_train" bigint NOT NULL,
+  "requested_train_owner" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "username" varchar UNIQUE NOT NULL,
+  "refresh_token" varchar NOT NULL,
+  "user_agent" varchar NOT NULL,
+  "client_ip" varchar NOT NULL,
+  "is_blocked" bool NOT NULL,
+  "expires_at" timestamptz NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE INDEX ON "trains" ("model_number");
+
+CREATE INDEX ON "trains" ("name");
+
+CREATE INDEX ON "collection_trains" ("user_id");
+
+CREATE INDEX ON "wishlist_trains" ("user_id");
+
+CREATE INDEX ON "trade_offers" ("offered_train_owner");
+
+CREATE INDEX ON "trade_offers" ("requested_train_owner");
+
+CREATE INDEX ON "trade_entry" ("offered_train_owner");
+
+CREATE INDEX ON "trade_entry" ("requested_train");
+
+CREATE INDEX ON "trade_entry" ("offered_train");
+
+CREATE INDEX ON "trade_entry" ("requested_train_owner");
+
+CREATE INDEX ON "sessions" ("id");
+
+ALTER TABLE "collection_trains" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "collection_trains" ADD FOREIGN KEY ("train_id") REFERENCES "trains" ("id");
+
+ALTER TABLE "wishlist_trains" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+ALTER TABLE "wishlist_trains" ADD FOREIGN KEY ("train_id") REFERENCES "trains" ("id");
+
+ALTER TABLE "trade_offers" ADD FOREIGN KEY ("offered_train") REFERENCES "trains" ("id");
+
+ALTER TABLE "trade_offers" ADD FOREIGN KEY ("offered_train_owner") REFERENCES "users" ("id");
+
+ALTER TABLE "trade_offers" ADD FOREIGN KEY ("requested_train") REFERENCES "trains" ("id");
+
+ALTER TABLE "trade_offers" ADD FOREIGN KEY ("requested_train_owner") REFERENCES "users" ("id");
+
+ALTER TABLE "trade_entry" ADD FOREIGN KEY ("offered_train") REFERENCES "trains" ("id");
+
+ALTER TABLE "trade_entry" ADD FOREIGN KEY ("offered_train_owner") REFERENCES "users" ("id");
+
+ALTER TABLE "trade_entry" ADD FOREIGN KEY ("requested_train") REFERENCES "trains" ("id");
+
+ALTER TABLE "trade_entry" ADD FOREIGN KEY ("requested_train_owner") REFERENCES "users" ("id");
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
